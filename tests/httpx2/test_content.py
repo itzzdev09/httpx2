9d4875724c5df28f4bec167f25f35d0588e69993
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-09-18 11:37:37 +0200

    Check file offsets without parametrizing the test

--- tests/httpx2/test_content.py
+++ tests/httpx2/test_content.py
@@ -70,17 +70,17 @@ async def test_bytesio_content() -> None:
     assert content == b"Hello, world!"
 
 
-@pytest.mark.parametrize("offset", [4, 10, 15])
-def test_file_content(offset: int) -> None:
+def test_file_content() -> None:
     with tempfile.TemporaryFile() as file:
         file.write(b"0123456789")
-        file.seek(offset)
-        expected = b"0123456789"[offset:]
+        for offset in (4, 10, 15):
+            file.seek(offset)
+            expected = b"0123456789"[offset:]
 
-        request = httpx2.Request(method, url, content=file)
-        assert file.tell() == offset
-        assert request.headers["Content-Length"] == str(len(expected))
-        assert request.read() == expected
+            request = httpx2.Request(method, url, content=file)
+            assert file.tell() == offset
+            assert request.headers["Content-Length"] == str(len(expected))
+            assert request.read() == expected
 
 
 def test_gzip_file_content() -> None:
